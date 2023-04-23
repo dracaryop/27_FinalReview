e7234e2b112a8973f6981e470f769978afbd99c8
Vaidik20346 <88655658+Vaidik20346@users.noreply.github.com>
2023-04-23 23:04:56 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Web-Crawler',
+    version = '1.0',
+    packages = [],
+    url = 'https://github.com/dracaryop/27_FinalReview',
+    license='',
+    author='anand',
+    author_email = 'anand20280@iiitd.ac.in',
+    description = '',
+    install_requires = ['bs4', 'nltk', 'scikit-learn', 'numpy']
+)
